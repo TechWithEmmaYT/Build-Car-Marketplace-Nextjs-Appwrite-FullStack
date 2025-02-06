940fea37e6ef9cf28089bc9783cadeabbb19ac49
Emmanuelbreezy <emmabreezyumeh@gmail.com>
2025-02-06 15:03:18 +0100

    🐛 (FIX: Space Issue)

--- components/CardCard.tsx
+++ components/CardCard.tsx
@@ -27,7 +27,6 @@ const CarCard: React.FC<CarCardProps> = ({ listing, layout = "grid" }) => {
     description,
   } = listing;
   const slug = createSlug(displayTitle);
-
   const conditionLabel = CAR_CONDITION_OPTIONS.find(
     (opt) => opt.value === condition
   )?.label;
